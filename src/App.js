079f28b9641d1e56321d9d975429c432a51c1daf
ragnarockerbunny <mdstevens0612@gmail.com>
2022-10-10 03:09:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import devpic from "./devpic.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className = "devpic" src = {devpic}/>
+      <h1>My Links:</h1>
+      <h4>By Matthew "ragnarockerbunny" Stevens</h4>
+      <ul>
+        <li><a target = "_blank" href = "https://github.com/ragnarockerbunny" rel="noreferrer noopener"><i className = "bi bi-github"></i> Github</a></li>
+        <li><a target = "_blank" href = "https://twitter.com/MatthewDaveStev" rel="noreferrer noopener"><i className = "bi bi-twitter"></i> Twitter</a></li>
+        <li><a target = "_blank" href = "https://www.youtube.com/channel/UCl8SdOVnxZzn7TmfNfzsRyQ" rel="noreferrer noopener"><i className = "bi bi-youtube"></i> YouTube</a></li>
+        <li><a target = "_blank" href = "https://howmattdavewrites.blogspot.com/" rel="noreferrer noopener"><i class="bi bi-journal-bookmark-fill"></i> Blog</a></li>
+      </ul>
     </div>
   );
 }
