db6de7206eecbeb873e114c18c297964b97a5331
Matthew Stevens <59891271+ragnarockerbunny@users.noreply.github.com>
2024-12-04 01:13:01 +0200

    Update App.js
    - Remove link to Twitter
    - Add links for website, Mastodon Profile, Bluesky Profile, Itch and Gamejolt

--- src/App.js
+++ src/App.js
@@ -9,9 +9,13 @@ function App() {
       <h4>By Matthew "ragnarockerbunny" Stevens</h4>
       <ul>
         <li><a target = "_blank" href = "https://github.com/ragnarockerbunny" rel="noreferrer noopener"><i className = "bi bi-github"></i> Github</a></li>
-        <li><a target = "_blank" href = "https://twitter.com/MatthewDaveStev" rel="noreferrer noopener"><i className = "bi bi-twitter"></i> Twitter</a></li>
+        <li><a target = "_blank" href = "https://rgb-dev.co.za/" rel="noreferrer noopener"><i className = "bi bi-globe"></i> My Website</a></li>
+        <li><a target = "_blank" href = "https://mas.to/@mdstevens0612" rel="noreferrer noopener"><i className="bi bi-mastodon"></i> Mastodon</a></li>
+        <li><a target = "_blank" href = "https://bsky.app/profile/mdstevens0612.bsky.social" rel="noreferrer noopener"><i className="bi bi-at"></i> BlueSky</a></li>
         <li><a target = "_blank" href = "https://www.youtube.com/channel/UCl8SdOVnxZzn7TmfNfzsRyQ" rel="noreferrer noopener"><i className = "bi bi-youtube"></i> YouTube</a></li>
-        <li><a target = "_blank" href = "https://howmattdavewrites.blogspot.com/" rel="noreferrer noopener"><i class="bi bi-journal-bookmark-fill"></i> Blog</a></li>
+        <li><a target = "_blank" href = "https://howmattdavewrites.blogspot.com/" rel="noreferrer noopener"><i className="bi bi-journal-bookmark-fill"></i> Blog</a></li>
+        <li><a target = "_blank" href = "https://ragnarockerbunny.itch.io/" rel="noreferrer noopener"><i className="bi bi-joystick"></i> Itch </li>
+        <li><a target = "_blank" href = "https://gamejolt.com/@ragnarockerbunny" rel="noreferrer noopener"><i className="bi bi-joystick"></i> Gamejolt </li>
       </ul>
     </div>
   );
